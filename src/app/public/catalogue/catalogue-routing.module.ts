be11b96ca72df59cbc4e7a4d1018cbcdd9987572
Gonik Daniel <gonik.daniel88@gmail.com>
2017-07-16 23:14:48 +0300

    Catalogue component

--- src/app/public/catalogue/catalogue-routing.module.ts
+++ src/app/public/catalogue/catalogue-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CatalogueComponent } from './catalogue.component';
+
 export const routes: Routes = [
+  {
+    path: '',
+    component: CatalogueComponent,
+    data: {
+      title: 'Catalogue'
+    }
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class CatalogueRoutingModule {}
+export class CatalogueRoutingModule { }
--- src/app/public/catalogue/catalogue.module.ts
+++ src/app/public/catalogue/catalogue.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CatalogueRoutingModule } from './catalogue-routing.module';
+import { CatalogueComponent } from './catalogue.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CatalogueRoutingModule
   ],
-  declarations: []
+  declarations: [CatalogueComponent]
 })
 export class CatalogueModule { }
