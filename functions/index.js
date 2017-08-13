e8cb731cfa41f1539968e7a91cc8e8e05bd31458
Gonik Daniel <gonik.daniel88@gmail.com>
2017-08-13 21:32:49 +0300

    Firebase functions - express app sample

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 const functions = require('firebase-functions');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.get('/favicon.ico', function(req, res) {
+  res.send(204);
+});
+
+app.get('/test', (req, res) => {
+  res.set('Cache-Control', 'public, max-age=60, s-maxage=180');
+  res.send(`${Date.now()}`);
+});
+
+exports.app = functions.https.onRequest(app);
