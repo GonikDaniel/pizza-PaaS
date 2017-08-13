ee452d6d8be036764526801b820268d2fdc98457
Gonik Daniel <gonik.daniel88@gmail.com>
2017-08-14 00:30:38 +0300

    User for navbar

--- src/app/admin/layout/navbar/navbar.component.ts
+++ src/app/admin/layout/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './../../../core/user/user.service';
 import { Component, OnInit } from '@angular/core';
 import { ROUTES } from '../routes/sidebar-routes.config';
 
@@ -7,12 +8,16 @@ import { ROUTES } from '../routes/sidebar-routes.config';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  public user;
   private listTitles: any[];
 
-  constructor() {
+  constructor(
+    private userService: UserService
+  ) {
   }
 
   ngOnInit() {
+    this.user = this.userService.user;
     this.listTitles = ROUTES.filter(listTitle => listTitle);
   }
 
