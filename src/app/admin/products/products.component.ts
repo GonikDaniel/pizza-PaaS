df99fc7e5058476f172cbe6c80904131486af325
Gonik Daniel <gonik.daniel88@gmail.com>
2017-08-17 01:00:55 +0300

    Modal updates

--- src/app/admin/products/products.component.ts
+++ src/app/admin/products/products.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, TemplateRef, ViewEncapsulation } from '@angular/core';
-import { BsModalService } from 'ngx-bootstrap/modal/bs-modal.service';
-import { ModalDirective } from 'ngx-bootstrap/modal/modal.component';
+// import { BsModalService } from 'ngx-bootstrap/modal/bs-modal.service';
 // import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 
 @Component({
@@ -11,10 +10,24 @@ import { ModalDirective } from 'ngx-bootstrap/modal/modal.component';
 })
 export class ProductsComponent implements OnInit {
   // public modalRef: BsModalRef;
-  public largeModal;
-  constructor(private modalService: BsModalService) {}
+  private value: any = ['pizza'];
+  public productTypes: Array<string> = ['pizza', 'salad', 'drink'];
+  public productName;
+  constructor() {}
 
   ngOnInit() {}
+
+  public selected(value:any):void {
+    console.log('Selected value is: ', value);
+  }
+
+  public removed(value:any):void {
+    console.log('Removed value is: ', value);
+  }
+
+  public refreshValue(value: any): void {
+    this.value = value;
+  }
 
   // public openModal(template: TemplateRef<any>) {
   //   this.modalRef = this.modalService.show(template);
