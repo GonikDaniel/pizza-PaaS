7b59070c8a31d7778b5f9e41774bfbf6c5244720
Gonik Daniel <gonik.daniel88@gmail.com>
2017-08-17 02:10:31 +0300

    Add product form updates

--- src/app/admin/products/products.component.ts
+++ src/app/admin/products/products.component.ts
@@ -12,7 +12,31 @@ export class ProductsComponent implements OnInit {
   // public modalRef: BsModalRef;
   private value: any = ['pizza'];
   public productTypes: Array<string> = ['pizza', 'salad', 'drink'];
+  public productImages: Array<string> = [
+    'amadeus.jpg',
+    'asianchicken.jpg',
+    'chickencoolness.jpg',
+    'gardengallop.jpg',
+    'grecoirish.jpg',
+    'holysmokes.jpg',
+    'hulapie.jpg',
+    'italianstallion.jpg',
+    'jerusalem.jpg',
+    'lisbon.jpg',
+    'littlevenus.jpg',
+    'makeyourown.jpg',
+    'mamamia.jpg',
+    'nocheese.jpg',
+    'pestolante.jpg',
+    'plaincheese.jpg',
+    'porkbbq.jpg',
+    'shanghai.jpg',
+    'southoftheborder.jpg',
+    'tajmahal.jpg',
+    'tomterrific.jpg'
+  ];
   public productName;
+  public productDescription;
   constructor() {}
 
   ngOnInit() {}
