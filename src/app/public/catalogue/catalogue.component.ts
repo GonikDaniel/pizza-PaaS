76869b35aaf81823566534fe0f1859da71da36fd
Gonik Daniel <gonik.daniel88@gmail.com>
2017-08-17 17:23:09 +0300

    About page and catalogue updates

--- src/app/public/catalogue/catalogue.component.ts
+++ src/app/public/catalogue/catalogue.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFireDatabase } from 'angularfire2/database';
+
 @Component({
   selector: 'paas-catalogue',
   templateUrl: './catalogue.component.html',
   styleUrls: ['./catalogue.component.scss']
 })
 export class CatalogueComponent implements OnInit {
+  public products;
 
-  constructor() { }
+  constructor(
+    private db: AngularFireDatabase
+  ) {
+    this.products = this.db.list('/products', {
+      query: {
+        limitToFirst: 20,
+        orderByChild: 'createdAt',
+        orderByKey: true
+      }
+    });
+  }
 
   ngOnInit() {
   }
