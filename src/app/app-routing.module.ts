b36cbe9f4262a1c47598c5e4a3164dec8bc1093e
Gonik Daniel <gonik.daniel88@gmail.com>
2017-08-18 00:15:27 +0300

    notfound component for non-matching routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,7 @@ export const routes: Routes = [
   { path: '', redirectTo: '/app', pathMatch : 'full' },
   { path: 'app', loadChildren: 'app/public/public.module#PublicModule' },
   { path: 'admin', canLoad: [AuthGuard], loadChildren: 'app/admin/admin.module#AdminModule' },
-  { path: '404', component: NotFoundComponent },
-  { path: '**', redirectTo: '/404', pathMatch : 'full' }
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
