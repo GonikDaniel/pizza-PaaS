60e64231ad586049bb2823a2e050df18381ae639
Gonik Daniel <gonik.daniel88@gmail.com>
2017-08-17 20:16:13 +0300

    Get cart - for any user

--- src/app/public/cart/cart.service.ts
+++ src/app/public/cart/cart.service.ts
@@ -5,22 +5,30 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { CookieService } from 'angular2-cookie/services/cookies.service';
 
+import * as firebase from 'firebase/app';
 import * as _ from 'lodash';
 
 @Injectable()
 export class CartService {
+  private cartsRef;
 
   constructor(
     private db: AngularFireDatabase,
     private userService: UserService,
     private cookieService: CookieService
-  ) {}
+  ) {
+    this.cartsRef = this.db.list('/carts');
+  }
 
   getCart() {
-    const cartId = _.get(this.userService.user, 'cartId', this.cookieService.get('tmpCartId'));
-    return cartId
-      ? this.db.object(`carts/${cartId}`)
-      : Observable.of({ items: [] });
+    let cartId = _.get(this.userService.user, 'cartId', this.cookieService.get('tmpCartId'));
+    if (!cartId) {
+      const createdAt = firebase.database.ServerValue.TIMESTAMP;
+      const newCartRef = this.cartsRef.push({ createdAt, items: [] });
+      cartId = newCartRef.key;
+      this.cookieService.put('tmpCartId', cartId);
+    }
+    return this.db.object(`carts/${cartId}`);
   }
 
 }
